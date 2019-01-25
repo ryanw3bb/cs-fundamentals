ee444a68ed0ae1aedd62931e0bd4323aa13aa996
Ryan Webb <ryan@ryanwebb.com>
2019-01-25 12:13:05 +0000

    Binary search and linear search

---
+++ Search Algorithms/Assets/Scripts/BinarySearch.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public static class BinarySearch
+{
+    public static int[] SearchArray(int[] sortedArray, int target)
+    {
+        int[] targetRange = { -1, -1 };
+
+        int leftIdx = ExtremeInsertionIndex(sortedArray, target, true);
+
+        // assert that `leftIdx` is within the array bounds and that `target`
+        // is actually in `nums`.
+        if(leftIdx == sortedArray.Length || sortedArray[leftIdx] != target)
+        {
+            return targetRange;
+        }
+
+        targetRange[0] = leftIdx;
+        targetRange[1] = ExtremeInsertionIndex(sortedArray, target, false) - 1;
+
+        return targetRange;
+    }
+
+    // returns leftmost (or rightmost) index at which target should be
+    // inserted in sorted array nums via binary search.
+    private static int ExtremeInsertionIndex(int[] nums, int target, bool left)
+    {
+        int lo = 0;
+        int hi = nums.Length;
+
+        while(lo < hi)
+        {
+            int mid = (lo + hi) / 2;
+
+            if(nums[mid] > target || (left && nums[mid] == target))
+            {
+                hi = mid;
+            }
+            else
+            {
+                lo = mid + 1;
+            }
+        }
+
+        return lo;
+    }
+
+    // returning the index of the first occurence we come to of given target
+    public static int IndexFind(int[] nums, int target)
+    {
+        int lo = 0;
+        int hi = nums.Length;
+
+        while(lo < hi)
+        {
+            int mid = (lo + hi) / 2;
+
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            if(nums[mid] > target)
+            {
+                hi = mid;
+            }
+            else
+            {
+                lo = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ Search Algorithms/Assets/Scripts/SearchAlgorithms.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchAlgorithms : MonoBehaviour 
+{
+    private const int MS_IN_SEC = 1000;
+
+    private List<SearchTestCase> testCases = new List<SearchTestCase>();
+
+    private void Start()
+    {
+        testCases.Add(new SearchTestCase(new int[] { 5, 7, 7, 8, 8, 10 }, 8));
+        testCases.Add(new SearchTestCase(new int[] { 3, 3, 3 }, 3));
+        testCases.Add(new SearchTestCase(new int[] { 1 }, 1));
+
+        double ms = 0;
+        float time = Time.realtimeSinceStartup;
+
+        // linear search
+        foreach(SearchTestCase testCase in testCases)
+        {
+            int[] result = LinearSearch.SearchArray(testCase.SortedArray, testCase.Target);
+            Debug.Log(string.Format("{0}:{1}", result[0], result[1]));
+        }
+
+        ms = Math.Round((Time.realtimeSinceStartup - time) * MS_IN_SEC, 2);
+        Debug.Log(string.Format("Linear: {0}ms", ms));
+        time = Time.realtimeSinceStartup;
+
+        // binary search
+        foreach(SearchTestCase testCase in testCases)
+        {
+            int[] result = BinarySearch.SearchArray(testCase.SortedArray, testCase.Target);
+            Debug.Log(string.Format("{0}:{1}", result[0], result[1]));
+        }
+
+        ms = Math.Round((Time.realtimeSinceStartup - time) * MS_IN_SEC, 2);
+        Debug.Log(string.Format("Binary: {0}ms", ms));
+        time = Time.realtimeSinceStartup;
+    }
+}
+
+public class SearchTestCase
+{
+    public int[] SortedArray;
+    public int Target;
+
+    public SearchTestCase(int[] sortedArray, int target)
+    {
+        SortedArray = sortedArray;
+        Target = target;
+    }
+}
